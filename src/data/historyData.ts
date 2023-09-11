ac58239b455715ec6a00166321ae2c508d7fdcdd
gorobchenko-k <gorobchenko.es@mail.ru>
2023-09-11 13:40:41 +0300

    feat: add history information

---
+++ src/data/historyData.ts
@@ -0,0 +1,78 @@
+import { HistoryData } from 'types';
+
+export const historyInfo: HistoryData[] = [
+  {
+    title: 'Кино',
+    startYear: 1987,
+    endYear: 1991,
+    events: {
+      1987: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1988: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      1989: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1990: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1991: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+  {
+    title: 'Литература',
+    startYear: 1992,
+    endYear: 1997,
+    events: {
+      1992: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1993: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1994: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1995: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1996: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      1997: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+  {
+    startYear: 1999,
+    endYear: 2004,
+    events: {
+      1999: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2000: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2001: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2002: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2003: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2004: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+  {
+    startYear: 2015,
+    endYear: 2022,
+    events: {
+      2015: '13 сентября — частное солнечное затмение, видимое в Южной Африке и части Антарктиды',
+      2016: 'Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11',
+      2017: 'Компания Tesla официально представила первый в мире электрический грузовик Tesla Semi',
+      2018: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2019: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2020: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2021: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+  {
+    title: '5title',
+    startYear: 2012,
+    endYear: 2014,
+    events: {
+      2012: 'Ввод в эксплуатацию очистных сооружений на «СЕРВОЛЮКС АГРО»',
+      2013: 'Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11',
+      2014: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+  {
+    title: '6title',
+    startYear: 2015,
+    endYear: 2022,
+    events: {
+      2015: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2016: 'Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных галактик, получившую обозначение GN-z11',
+      2017: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2018: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2019: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2020: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+      2021: 'Iusto, quae. Nihil quis quasi nesciunt asperiores.',
+    },
+  },
+];
